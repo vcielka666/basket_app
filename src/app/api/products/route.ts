baaac71a34adb5ecbdfff79f17ac12d5909f881e
vcielka666 <romanmarko90@gmail.com>
2024-07-21 18:56:26 +0200

    Corrected code

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,39 +1,22 @@
-// src/pages/api/products/index.ts
-import { NextApiRequest, NextApiResponse } from 'next';
-import prisma from '../../../../lib/prisma';
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "../../../../lib/prisma";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
-    try {
-      const { shopCenterName, productName, productWeight, productPrice } = req.body;
-      console.log("Received data:", { shopCenterName, productName, productWeight, productPrice });
+export async function POST(req: NextRequest) {
+  const { shopCenterName, productName, productWeight, productPrice } = await req.json();
 
-      const product = await prisma.products.create({
-        data: {
-          shopCenterName,
-          productName,
-          productWeight,
-          productPrice,
-        },
-      });
+  const product = await prisma.products.create({
+    data: {
+      shopCenterName,
+      productName,
+      productWeight,
+      productPrice,
+    },
+  });
 
-      console.log("Created product:", product);
-      res.status(200).json(product);
-    } catch (error) {
-      console.error("Error creating product:", error);
-      res.status(500).json({ error: "Error creating product" });
-    }
-  } else if (req.method === 'GET') {
-    try {
-      const products = await prisma.products.findMany();
-      console.log("Fetched products:", products);
-      res.status(200).json(products);
-    } catch (error) {
-      console.error("Error fetching products:", error);
-      res.status(500).json({ error: "Error fetching products" });
-    }
-  } else {
-    res.setHeader('Allow', ['GET', 'POST']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
+  return NextResponse.json(product);
+}
+
+export async function GET(){
+  const products = await prisma.products.findMany();
+  return NextResponse.json(products);
 }
