81939c129c217f3be0b25feafe0c87dbb4e144eb
vcielka666 <romanmarko90@gmail.com>
2024-07-21 17:28:22 +0200

    The Basket App

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import ProductsManager from "./components/ProductsManager";
 const HomePage = () => {
   return (
     <div className='flex items-center flex-col size-10 w-screen p-10 m-10 box-border'>
-      <h1>Basket App</h1>
+      <h1>THE BASKET APP</h1>
       <ProductsManager />
     </div>
   );
